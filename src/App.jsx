933aae156e6804e3995bf4aa3b0a4a063fabe96e
Abhinay Kukkadapu <akukkada@qti.qualcomm.com>
2020-02-02 19:03:04 -0800

    Commiting changes to intro

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ export default class App extends Component {
           <div className="container-inner-wrapper">
             <h1>Deepthi Rachakonda</h1>
             <img src="./pic.jpg" className="pic" alt="logo"/>
-            <p>Hi everyone, I'm Deepthi Rachakonda, i'm interested in learning upcoming technologies that included various CS fields such as front end, back end web technologies, data visualization etc. I've worked with Metrix labs as data analyst for 2 years. I enjoy cooking, hiking in my free time. </p>
+            <p>Hi everyone, I'm Deepthi Rachakonda, i'm interested in learning upcoming technologies that includes various CS fields such as front end, back end web technologies, data visualization etc. I've worked with Metrix labs as data analyst for 2 years. I enjoy cooking, hiking in my free time. </p>
             <a className="button" href="https://github.com/deepthirachakonda">View my github Profile</a>
           </div>
         </div>
